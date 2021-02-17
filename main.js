9cd8346045e74a1877cfdba7e9053c2f58179243
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2021-02-17 19:50:33 +0100

    JS added for navbar link functionality.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+"use strict";
 
+// Section - Navbar
+
+const activateLink1 = () => {
+  document.getElementById("link1").classList.add("navbar__link--active");
+  document.getElementById("link2").classList.remove("navbar__link--active");
+  document.getElementById("link3").classList.remove("navbar__link--active");
+};
+
+const activateLink2 = () => {
+  document.getElementById("link2").classList.add("navbar__link--active");
+  document.getElementById("link1").classList.remove("navbar__link--active");
+  document.getElementById("link3").classList.remove("navbar__link--active");
+};
+
+const activateLink3 = () => {
+  document.getElementById("link3").classList.add("navbar__link--active");
+  document.getElementById("link2").classList.remove("navbar__link--active");
+  document.getElementById("link1").classList.remove("navbar__link--active");
+};
