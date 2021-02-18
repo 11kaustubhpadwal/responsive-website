7cffb3bd2062f6c96bb7aef1275dde1f77f8a53a
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2021-02-18 18:38:01 +0100

    Desktop CSS completed.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Section - Navbar
+
+const activateLink1 = () => {
+  document.getElementById("link1").classList.add("navbar__link--active");
+  document.getElementById("link2").classList.remove("navbar__link--active");
+  document.getElementById("link3").classList.remove("navbar__link--active");
+};
+
+const activateLink2 = () => {
+  document.getElementById("link2").classList.add("navbar__link--active");
+  document.getElementById("link1").classList.remove("navbar__link--active");
+  document.getElementById("link3").classList.remove("navbar__link--active");
+};
+
+const activateLink3 = () => {
+  document.getElementById("link3").classList.add("navbar__link--active");
+  document.getElementById("link2").classList.remove("navbar__link--active");
+  document.getElementById("link1").classList.remove("navbar__link--active");
+};
+
+// Section - Team
+
+const changeCurrentPortion = () => {};
+
+// Section - Contact
+
+const activateCheckbox = () => {
+  if (
+    !document
+      .getElementById("checkbox")
+      .classList.contains("contact__form--input-checkbox")
+  ) {
+    document
+      .getElementById("checkbox")
+      .classList.add("contact__form--input-checkbox");
+  } else {
+    document
+      .getElementById("checkbox")
+      .classList.remove("contact__form--input-checkbox");
+  }
+};
